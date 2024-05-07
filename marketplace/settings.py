6b46988ac59f717c270cd8556c752d722bcc0f62
arsidd78 <arsidd78@gmail.com>
2024-05-07 16:17:10 +0500

    Login Logic and Home page created

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'registration.apps.RegistrationConfig',
     'product.apps.ProductConfig',
-    'memeber.apps.MemberConfig',
+    'member.apps.MemberConfig',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- registration/views.py
+++ registration/views.py
@@ -1,1 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+# Views:
+def authentication (request):
+    if request.method == 'POST':
+        user_name = request.POST.get('username')
+        password = request.POST.get('password')
+        user = User(request, username=user_name,password=password ) # authenticating user
+        if user is not None:
+            login(request,user) # This will log in a user
+            return redirect(request,'home') # Redirect to home page
+        else:
+            msg = messages.add_message(request, messages.ERROR, "Invalid email or password")
+            return render(request,'registration/login.html') # If credentials are invalid than it will render login page again with an error message
+    else:
+        return redirect('home')
+    
+def login (request):
+    return render(request,'registration/login.html')
