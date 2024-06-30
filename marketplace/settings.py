8261ee5b4781e8ff495ed321e00e5bfadde8b8df
arsidd78 <arsidd78@gmail.com>
2024-06-30 13:57:45 +0500

    DEPLOYING..

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-edtcd^w6h6pu9@0^9j^5-03j9e9it$h091a%cw!c-$)%!#+uf-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
